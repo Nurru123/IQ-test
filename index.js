3d5680168b2f1a4cb7372bc48a34ab0df142d907
Nurru123 <little.emiliya@gmail.com>
2022-03-04 14:12:39 +0300

    selectors changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const details = document.querySelector('.details_arrow');
 const btnTest = document.querySelector('.btn-test');
-const contentHidden = document.querySelector('.main__content_hidden');
+const contentHidden = document.querySelector('.main-page__content_hidden');
 const menuBtn = document.querySelector('.sandwich');
-const menuCover = document.querySelector('.main__menu_cover');
+const menuCover = document.querySelector('.main-page__menu');
 const exitBtn = document.querySelector('.menu_exit-btn');
 
 details.addEventListener('click', showHideDetails);
