2605664345e56a118aa5d923ee2aa83bf812a80c
Nurru123 <little.emiliya@gmail.com>
2022-03-05 13:18:00 +0300

    more adaptive

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const menuCover = document.querySelector('.main-page__menu');
 const exitBtn = document.querySelector('.menu_exit-btn');
 const testBtn = document.querySelectorAll('.btn-test')
 
-details.addEventListener('click', showHideDetails);
+// details.addEventListener('click', showHideDetails);
 menuBtn.addEventListener('click', showHideMenu);
 exitBtn.addEventListener('click', showHideMenu);
 testBtn.forEach(btn => (
@@ -16,18 +16,18 @@ testBtn.forEach(btn => (
 ))
 
 
-function showHideDetails() {
-    contentHidden.classList.toggle('hidden')
-    if (!contentHidden.classList.contains('hidden')) {
-        details.classList.add('arrow-up')
-    } else details.classList.remove('arrow-up')
-}
+// function showHideDetails() {
+//     contentHidden.classList.toggle('hidden')
+//     if (!contentHidden.classList.contains('hidden')) {
+//         details.classList.add('arrow-up')
+//     } else details.classList.remove('arrow-up')
+// }
 
 function showHideMenu() {
     menuCover.classList.toggle('show-menu');
-    if (!contentHidden.classList.contains('hidden')) {
-        console.log('pook')
-        contentHidden.classList.add('hidden')
-        details.classList.remove('arrow-up')
-    }
+    // if (!contentHidden.classList.contains('hidden')) {
+    //     console.log('pook')
+    //     contentHidden.classList.add('hidden')
+    //     details.classList.remove('arrow-up')
+    // }
 }
