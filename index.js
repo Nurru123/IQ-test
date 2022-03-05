224e3615537821bebc2ff46b413788b7d8b9f9a4
Nurru123 <little.emiliya@gmail.com>
2022-03-05 12:49:37 +0300

    adaptive

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const contentHidden = document.querySelector('.main-page__content');
 const menuBtn = document.querySelector('.sandwich');
 const menuCover = document.querySelector('.main-page__menu');
 const exitBtn = document.querySelector('.menu_exit-btn');
+const testBtn = document.querySelectorAll('.btn-test')
 
 details.addEventListener('click', showHideDetails);
 menuBtn.addEventListener('click', showHideMenu);
 exitBtn.addEventListener('click', showHideMenu);
+testBtn.forEach(btn => (
+    btn.addEventListener('click', function() {
+        window.location.href = './q1/q1.html'
+    })
+))
 
 
 function showHideDetails() {
     contentHidden.classList.toggle('hidden')
     if (!contentHidden.classList.contains('hidden')) {
         details.classList.add('arrow-up')
-    }
+    } else details.classList.remove('arrow-up')
 }
 
 function showHideMenu() {
     menuCover.classList.toggle('show-menu');
     if (!contentHidden.classList.contains('hidden')) {
+        console.log('pook')
         contentHidden.classList.add('hidden')
         details.classList.remove('arrow-up')
     }
